fe24a235bde1dc185689f8c6940b5dd12569e9fd
ehawk823 <evan.alexander.hawk@gmail.com>
2015-11-11 23:25:43 -0600

    started to build out lawyer and entrepreneur pathways

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,17 @@
 class CompaniesController < ApplicationController
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    Company.create(params[:company])
+  end
+
+  private
+
+  def person_params
+    params.require(:company).permit(:name, :description, :challenges)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
 
-  def 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    current_user.name = params[:user][:name]
+    current_user.location = params[:user][:location]
+    current_user.title = params[:user][:title]
+    current_user.save
+    redirect_to '/companies/new'
+  end
+
 end
